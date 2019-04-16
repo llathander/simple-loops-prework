7e7ef7eae9d5feeccaec45a76503a24100810dab
Adrian Samaniego <llathander@gmail.com>
2019-04-16 23:19:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,7 +6,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
     count =+ count 
-    break if count 
+    break if count == 7
   end
 end
 
